4166cc3624318a1ed7a62d93291991dd1944341c
Adithya Yerramsetty <adithyay@hotmail.com>
2023-07-21 13:21:39 -0700

    Changed visibility of all internal variables to make them accessible by client code. Helps with debugging.

--- src/pythongraphrunner/TaskGraph.py
+++ src/pythongraphrunner/TaskGraph.py
@@ -69,37 +69,37 @@ class TaskEdge(Generic[T]):
     is met
   """
   def __init__(self, startStates : List[str], endStates : List[str], errorEndStates : List[str], runner : Callable[ [T], str], id : Optional[str] = None):
-    self.__startStates = startStates
-    self.__endStates = endStates
-    self.__errorEndStates = errorEndStates
-    self.__runner = runner
+    self._startStates = startStates
+    self._endStates = endStates
+    self._errorEndStates = errorEndStates
+    self._runner = runner
 
-    self.__id : str = id if id is not None else uuid.uuid4().hex
+    self._id : str = id if id is not None else uuid.uuid4().hex
 
     # A set for valid outputs
-    self.__validOutputs : Set[str] = set( self.__endStates + self.__errorEndStates )
+    self._validOutputs : Set[str] = set( self._endStates + self._errorEndStates )
   
   # Getters for states
   @property
   def startStates(self) -> List[str]:
-    return self.__startStates
+    return self._startStates
   
   @property
   def endStates(self) -> List[str]:
-    return self.__endStates
+    return self._endStates
 
   @property
   def errorEndStates(self) -> List[str]:
-    return self.__errorEndStates
+    return self._errorEndStates
 
   # Getters for the id and runner
   @property
   def runner(self) -> Callable[ [T], str]:
-    return self.__runner
+    return self._runner
 
   @property
   def id(self) -> str:
-    return self.__id
+    return self._id
 
   def __call__(self, o : T) -> str:
     """
@@ -107,9 +107,9 @@ class TaskEdge(Generic[T]):
     and verifies we exit into a valid
     state.
     """
-    outState = self.__runner(o)
+    outState = self._runner(o)
 
-    if outState not in self.__validOutputs:
+    if outState not in self._validOutputs:
       raise ValueError(f"Invalid output state {outState}!")
     else:
       return outState
@@ -123,24 +123,24 @@ class TaskGraph(Generic[T]):
   """
   def __init__(self):
     # Constructing our graph
-    self.__graph = nx.DiGraph()
+    self._graph = nx.DiGraph()
     
     # Contains a set of states we allow
-    self.__states : Set[str] = set()
+    self._states : Set[str] = set()
 
     # For right now, I'll expirement with the task
     # graph owning the items. Will see if this works
-    self.__items : Dict[str, T] = {}
+    self._items : Dict[str, T] = {}
 
     # Maintains a set of discrepant items
-    self.__discrepantItems : Set[str] = set()
+    self._discrepantItems : Set[str] = set()
     
   def addState(self, stateName : str):
     """
     Adds a state to the graph
     """
-    self.__states.add(stateName)
-    self.__graph.add_node(stateName)
+    self._states.add(stateName)
+    self._graph.add_node(stateName)
   
   def addStates(self, stateNames : List[str]):
     """
@@ -155,19 +155,19 @@ class TaskGraph(Generic[T]):
     """
     # Check if the states are valid
     for state in taskEdge.startStates + taskEdge.endStates + taskEdge.errorEndStates:
-      if state not in self.__states:
+      if state not in self._states:
         raise ValueError(f"State {state} is not in the graph!")
     
     # Add the edge, as a node
-    self.__graph.add_node(taskEdge.id, edgeObj = taskEdge)
+    self._graph.add_node(taskEdge.id, edgeObj = taskEdge)
 
     # Add all edges to the node that aren't error edges
     for startState in taskEdge.startStates:
-      self.__graph.add_edge(startState, taskEdge.id)
+      self._graph.add_edge(startState, taskEdge.id)
     
     # Add all end states to the node
     for endState in taskEdge.endStates:
-      self.__graph.add_edge(taskEdge.id, endState)
+      self._graph.add_edge(taskEdge.id, endState)
   
   def addEdges(self, taskEdges : List[TaskEdge[T]]):
     """
@@ -181,77 +181,77 @@ class TaskGraph(Generic[T]):
     Adds an item to the graph
     """
     # Check if the item is already in the graph
-    if item.id in self.__items:
+    if item.id in self._items:
       raise ValueError(f"Item with id {item.id} is already in the graph!")
     
     # Add the item to the graph
-    self.__items[item.id] = item
+    self._items[item.id] = item
 
     # Check if the item is discrepant, and
     # add it to the set if needed
     if item.isDiscrepant:
-      self.__discrepantItems.add(item.id)
+      self._discrepantItems.add(item.id)
     
   def removeItem(self, itemID : str):
     """
     Removes an item from the graph
     """
     # Check if the item is in the graph
-    if itemID not in self.__items:
+    if itemID not in self._items:
       raise ValueError(f"Item with id {itemID} is not in the graph!")
     
     # Remove the item from the graph
-    self.__items.pop(itemID)
+    self._items.pop(itemID)
 
     # Check if the item is discrepant, and
     # remove it from the set if needed
-    if itemID in self.__discrepantItems:
-      self.__discrepantItems.remove(itemID)
+    if itemID in self._discrepantItems:
+      self._discrepantItems.remove(itemID)
   
   def getItemCurrState(self, itemID : str) -> str:
-    return self.__items[itemID].currState
+    return self._items[itemID].currState
 
   def getItemDesiredState(self, itemID : str) -> str:
-    return self.__items[itemID].desiredState
+    return self._items[itemID].desiredState
 
   def getItem(self, itemID : str) -> T:
-    return self.__items[itemID]
+    return self._items[itemID]
   
   def getItemIDs(self) -> List[str]:
-    return list(self.__items.keys())
+    return list(self._items.keys())
   
   def getItems(self) -> List[ Tuple[str, T] ]:
-    return list(self.__items.items())
+    return list(self._items.items())
   
   def updateItemStates(self, itemID : str, currState : str, desiredState : str):
-    if itemID not in self.__items:
+    if itemID not in self._items:
       raise ValueError(f"Item with id {itemID} is not in the graph!")
   
     # Update the item's states
-    self.__items[itemID].currState = currState
-    self.__items[itemID].desiredState = desiredState
+    self._items[itemID].currState = currState
+    self._items[itemID].desiredState = desiredState
 
     # Check if the item is discrepant, and
     # add it to the set if needed
-    if self.__items[itemID].isDiscrepant and itemID not in self.__discrepantItems:
-      self.__discrepantItems.add(itemID)
-    elif not self.__items[itemID].isDiscrepant and itemID in self.__discrepantItems:
-      self.__discrepantItems.remove(itemID)
+    if self._items[itemID].isDiscrepant and itemID not in self._discrepantItems:
+      self._discrepantItems.add(itemID)
+    elif not self._items[itemID].isDiscrepant and itemID in self._discrepantItems:
+      self._discrepantItems.remove(itemID)
   
   def fixItems(self):
     """
     For all discrepant items, run the
     appropriate transition
     """
-    for itemID in self.__discrepantItems:
+    for itemID in self._discrepantItems:
       # Get the item
-      item = self.__items[itemID]
+      item = self._items[itemID]
       startState = item.currState
       endState = item.desiredState
       
       # Determine the shortest path between
       # the current state and desired
-      path : list = list(nx.shortest_path( self.__graph, startState, endState))
+      path : list = list(nx.shortest_path( self._graph, startState, endState))
 
       # The index we're currently at. If
       # the state we enter into, which
@@ -269,7 +269,7 @@ class TaskGraph(Generic[T]):
       # change the destination to where you actually
       # want to go
       while idx < len(path) - 1:
-        nextState = self.__graph.nodes[path[idx]]["edgeObj"] (item)
+        nextState = self._graph.nodes[path[idx]]["edgeObj"] (item)
 
         # Set the item's curr state
         item.currState = nextState
@@ -284,4 +284,4 @@ class TaskGraph(Generic[T]):
           idx -= idx + 1
 
           path.clear()
-          path = path + list(nx.shortest_path( self.__graph, startState, endState ))
+          path = path + list(nx.shortest_path( self._graph, startState, endState ))
